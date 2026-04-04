d5ff6f8bfa16ea037e72c1d914bff94c2992a5b9
Florian Kaiser <mail@floriankaiser.org>
2026-04-05 00:16:43 +0200

    Fix favicon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,8 @@
 <script lang="ts">
 import './layout.css'
-import favicon from '$lib/assets/favicon.svg'
-
 let { children } = $props()
 </script>
 
-<svelte:head>
-  <link rel="icon" href={favicon} />
-  <meta name="theme-color" content="#1e293b" />
-</svelte:head>
 
 <div class="h-screen bg-white text-gray-900 dark:bg-gray-950 dark:text-gray-100">
   {@render children()}
